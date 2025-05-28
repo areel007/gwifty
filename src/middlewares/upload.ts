bfb1d9273caba354a5cf890268cbf037c9c9db7b
bamidele <morenikejisunday12@gmail.com>
2025-05-29 00:49:20 +0100

    implement gift card endpoints

---
+++ src/middlewares/upload.ts
@@ -0,0 +1,4 @@
+// middlewares/upload.ts
+import multer from "multer";
+
+export const upload = multer({ storage: multer.memoryStorage() });
---
+++ src/routes/gift_card/index.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { authenticate, authorize } from "../../middlewares/auth";
+import {
+  createGiftCard,
+  deleteCard,
+  getGiftCards,
+  getGiftCardsBySeller,
+  getSellerOfAGiftCard,
+} from "../../controllers/gift-card";
+import { upload } from "../../middlewares/upload";
+
+const router = Router();
+
+router
+  .route("/create")
+  .post(
+    authenticate,
+    authorize("user", "admin"),
+    upload.single("imageUrl"),
+    createGiftCard
+  );
+
+router.route("/all-gift-cards").get(getGiftCards);
+router
+  .route("/my-gift-cards")
+  .get(authenticate, authorize("user", "admin"), getGiftCardsBySeller);
+router
+  .route("/delete/:id")
+  .delete(authenticate, authorize("user", "admin"), deleteCard);
+router
+  .route("/get-seller-of-gift-card/:id")
+  .get(authenticate, authorize("admin"), getSellerOfAGiftCard);
+
+export default router;

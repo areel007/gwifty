2c8ae5521392c217da7d36a9c7e6beeab6f56603
bamidele <morenikejisunday12@gmail.com>
2025-07-19 18:22:11 +0100

    minor fix

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // your frontend origin
+    origin: "https://gwifty.vercel.app/", // your frontend origin
     credentials: true, // ✅ allow cookies
   })
 );

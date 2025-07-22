459940efd2e6d1d348da0f2b6eff49dea1faf47f
bamidele <morenikejisunday12@gmail.com>
2025-07-22 14:45:32 +0100

    final minor fix

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 const corsOptions = {
   origin: [
     "http://localhost:3000", // ✅ dev
-    "https://cryptowise-client.vercel.app/", // ✅ deployed frontend
+    "https://cryptowise-client.vercel.app", // ✅ deployed frontend
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
